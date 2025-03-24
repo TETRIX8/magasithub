f5be51326055981980dc19fa63cc034f56114c56
AK project <132359965+TETRIX8@users.noreply.github.com>
2025-03-24 16:32:28 +0300

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -15,9 +15,10 @@ const NotFound = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
+        <p className="text-xl text-gray-600 mb-4">мне лень дописывать потом чтото добавлю</p>
+        <p className="text-sm text-gray-500 mb-6">Создано Проектом akproject</p>
         <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
+          вернись да на главную страницу дик ма вий из
         </a>
       </div>
     </div>
